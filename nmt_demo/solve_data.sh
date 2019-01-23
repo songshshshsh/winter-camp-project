b328eb7b69b8747b64dfc0d1985a9f85129add18
YuSihao1993 <747289326@qq.com>
2019-01-23 16:16:20 +0000

    scripts for task

--- nmt_demo/solve_data.sh
+++
@@ -1,5 +0,0 @@
-#python src/total_preprocess.py ./data
-#python src/find_best_pair.py ./data/process INTJ INFJ
-mkdir -p ./data/datasets/INTJ2INFJ
-python src/make_dataset.py ./data/process/ ./data/datasets/INTJ2INFJ/ INTJ INFJ
-
