4e194d2316d8bf8141cac759886001c310a95b65
songshshshsh <644240545@qq.com>
2019-01-24 15:42:21 +0800

    remove server

--- server/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import os
-
-app = Flask(__name__, static_folder='dist', template_folder='dist', static_url_path='/dist')
-
-from router import *
-
-if __name__ == '__main__':
-    app.run()
